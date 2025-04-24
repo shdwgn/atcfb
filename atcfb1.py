9619bf30734a39d5f21dd1f8f32f85dd8a8640df
shdwgn <jirorosc@gmail.com>
2025-04-24 11:09:37 +0800

    Update and rename CreateV0.5 (2).py to atcfb1.py

--- atcfb1.py
+++ atcfb1.py
@@ -556,12 +556,11 @@ logo=("""
           [green_yellow]THIS [dark_olive_gre]TOOLS [pale_green1] IS[dark_sea_green…] NOT FOR FREE
 """)
 ll=str([hari,tanggal])
-hx=("""  [bold green1]DEVELOPER[medium_purple1]   ⟩[cyan][bold] Deen
-  [bold green1]FACEBOOK[medium_purple1]    ⟩[cyan][bold] DEEN ABUEVA
-  [bold green1]GITHUB[medium_purple1]      ⟩[bright_yellow] Dian2025.git
-  [bold green1]STATUS[medium_purple1]      ⟩[bright_yellow] ALL NETWORK 
+hx=("""  [bold green1]DEVELOPER[medium_purple1]   ⟩[cyan][bold] ICHIRO
+  [bold green1]FACEBOOK[medium_purple1]    ⟩[cyan][pink] ICHIRO MIYAZATO
+  [bold green1]WIFEY[medium_purple1]       ⟩[cyan][pink] JANNA SAMSON
   [bold green1]TOOLS[medium_purple1]       ⟩[bright_yellow] PREMIUM 
-  [bold green1]UPDATES[medium_purple1]     ⟩[bright_yellow] MARCH 22
+  [bold green1]UPDATES[medium_purple1]     ⟩[bright_yellow] APRIL 24
   [bold green1]TODAY DATE[medium_purple1]  ⟩ [cyan]"""+ll)
 def banner():
     os.system("clear")
@@ -811,7 +810,7 @@ def confirm_id(mail,uid,otp,data,ses):
 #──────────────{ PROGRES }──────────────#
 def progres(current, num_accounts, delay):
 		for sleep in range(int(num_accounts), 0, -1):
-			print(f'[YUSH]-[{current}|{num_accounts}]-[SUCCESS:-{len(oks)}|BAD:-{len(cps)}]',end='\r')
+			print(f'[J4KENZO]-[{current}|{num_accounts}]-[SUCCESS:-{len(oks)}|BAD:-{len(cps)}]',end='\r')
 			time.sleep(1)
 			if current == num_accounts:
 				break
