47cd09e63f281fa8cf829d6aea548a66523edd08
shdwgn <jirorosc@gmail.com>
2025-04-25 15:06:58 +0800

    Update atcfb4.py

--- atcfb4.py
+++ atcfb4.py
@@ -1223,7 +1223,6 @@ def menu():
     print(Panel(a,subtitle="[bold purple]┌─",subtitle_align='left',style="bold purple"))
     bryxpassword = console.input("   [bold purple]└──> ")
     if bryxpassword in ["c","C","3","03"]:
-    banner()
     print(Panel(f" [bold green]ACCOUNT CREATING STARTED",style="bold purple"))
     print(Panel(f" [bold green]IF NO RESULT ON/OFF AIRPLANE MODE OR VPN 1.1.1.1",style="bold purple"))
     for _ in range(num_accounts):        
